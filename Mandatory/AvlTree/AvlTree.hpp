1099974daf3fca3674beb67540403610aa4103c3
ZAKARIA RABHI <zrabhi@student.1337.ma>
2023-01-26 16:11:50 +0100

    working on map

--- Mandatory/AvlTree/AvlTree.hpp
+++ Mandatory/AvlTree/AvlTree.hpp
@@ -6,7 +6,7 @@
 /*   By: zrabhi <zrabhi@student.1337.ma >           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 18:47:43 by zrabhi            #+#    #+#             */
-/*   Updated: 2023/01/24 20:14:37 by zrabhi           ###   ########.fr       */
+/*   Updated: 2023/01/26 15:25:48 by zrabhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,47 +21,55 @@
 
 namespace ft {
 template<class T, class Allocatore = std::allocator<T> >
-class AvlTree
+class AvlTree 
 {
     private :
+        typedef              T                              value_type;
+        typedef             Allocatore                      allocator_type;
+        typedef             std::size_t                     size_type; 
+        typedef typename    allocator_type::reference       reference;
+        typedef typename    allocator_type::const_reference const_reference;
+        typedef typename    allocator_type::pointer         pointer;
+        typedef typename    allocator_type::const_pointer   const_pointer;
       struct node
       {
-        T key;
-        node *left;
-        node *right;
-      }; 
+        value_type   __key;
+        node        *__left;
+        node        *__right;
+      };
+       
+    typedef  typename allocator_type::template rebind<node>::other  node_pointer;
          
-    node*    AddLeafPrivate(int key, node *_ptr)
+    node*    AddLeafPrivate(value_type key, node *_ptr)
     {
         if (!_ptr)
             return (creatleaf(key));
-        else if (key < _ptr->key)
-                _ptr->left = AddLeafPrivate(key, _ptr->left);
-        else if (key >_ptr->key )
-                _ptr->right = AddLeafPrivate(key, _ptr->right);
+        else if (key < _ptr->__key->second)
+                _ptr->__left = AddLeafPrivate(key, _ptr->__left);
+        else if (key >_ptr->__key->second )
+                _ptr->__right = AddLeafPrivate(key, _ptr->__right);
         else
             return _ptr;
-        
         return checklBalance(key, _ptr);
     }
 
-    node    *checklBalance(int key, node* _ptr)
+    node    *checklBalance(value_type key, node* _ptr)
     {
         int height = CalculHieghtHeight(_ptr);
-        if (height > 1 && key < _ptr->left->key)
+        if (height > 1 && key < _ptr->__left->__key->second)
                 return rightRotate(_ptr);
 
-        if (height < -1 && key > _ptr->right->key)
+        if (height < -1 && key > _ptr->__right->__key->second)
           return  leftRotate(_ptr);
-        if (height > 1 && key > _ptr->left->key)
+        if (height > 1 && key > _ptr->__left->__key->second)
         {
-          _ptr->left = leftRotate(_ptr->left);
+          _ptr->__left = leftRotate(_ptr->__left);
           return rightRotate(_ptr);
         }
     
-        if (height < -1 && key < _ptr->right->key)
+        if (height < -1 && key < _ptr->__right->__key->second)
         {
-            _ptr->right = rightRotate(_ptr->right);
+            _ptr->__right = rightRotate(_ptr->__right);
             return leftRotate(_ptr);
         }
         return _ptr;
@@ -72,22 +80,22 @@ class AvlTree
         if (!root)
             return;
         space += 10;
-        PrintTreePrivate(root->right, space);
+        PrintTreePrivate(root->__right, space);
         std::cout << std::endl;
         for(size_t i = 0; i < space; i++)  std::cout << " ";
-        std::cout << root->key << std::endl;
-        PrintTreePrivate(root->left, space);
+        std::cout << root->__key->second << std::endl;
+        PrintTreePrivate(root->__left, space);
     }
     
     void    PrintInOrderPrivate(node *ptr)
     {
         if (root)
         {
-            if (ptr->left)
-                PrintInOrderPrivate(ptr->left);
-            std::cout << ptr->key << " "; 
-            if (ptr->right)
-                PrintInOrderPrivate(ptr->right);
+            if (ptr->__left)
+                PrintInOrderPrivate(ptr->__left);
+            std::cout << ptr->__key->second << " "; 
+            if (ptr->__right)
+                PrintInOrderPrivate(ptr->__right);
         }
         else
             std::cout << "Tree is empty!" << std::endl;     
@@ -95,14 +103,9 @@ class AvlTree
   
     int     FindSmallestPrivate(node* ptr) // same ass subtree function
     {
-        // if (!root)
-        //    return ( std::cout << "Tree is Empty!" << std::endl, -1); //// still need some optimization
-        // else
-        // {
-            if (ptr->left)
-                return (FindSmallestPrivate(ptr->left));
-            return (ptr->key);
-        // }
+        if (ptr->__left)
+            return (FindSmallestPrivate(ptr->__left));
+        return (ptr->__key->second);
     }
 
     int     FindOldestPrivate(node *ptr)
@@ -111,69 +114,73 @@ class AvlTree
             return ( std::cout << "Tree is Empty!" << std::endl, -1);
         else
         {
-            if (ptr->right)
-                return (FindOldestPrivate(ptr->right));
-            return (ptr->key);
+            if (ptr->__right)
+                return (FindOldestPrivate(ptr->__right));
+            return (ptr->__key->second);
         }
     }
-    
+    bool    empty()
+    {
+        if (!root)
+            return (true);
+        return (false);
+    }
     int     smallestInSubTree(node *_ptr)
     {    
-        if (_ptr->left)
-            return (smallestInSubTree(_ptr->left));
-        return (_ptr->key);
+        if (_ptr->__left)
+            return (smallestInSubTree(_ptr->__left));
+        return (_ptr->__key->second);
     }
     
-    node   *deleteNode(node *_ptr, int key)
+    node   *deleteNode(node *_ptr, value_type key)
     {
         if (_ptr == NULL)
             return NULL;
-    else if (key < _ptr -> key) 
-      _ptr->left = deleteNode(_ptr->left, key);
-    else if (key > _ptr->key) {
-      _ptr-> right = deleteNode(_ptr-> right, key);
+    else if (key < _ptr->__key->second) 
+      _ptr->__left = deleteNode(_ptr->__left, key);
+    else if (key > _ptr->__key->second) {
+      _ptr->__right = deleteNode(_ptr->__right, key);
     }
     else
     {
-      if (_ptr->left == NULL)
-      {
-        node * temp = _ptr-> right;
-        delete _ptr;
-        return temp;
-      } 
-      else if (_ptr->right == NULL)
-      {
-        node * temp = _ptr-> left;
-        delete _ptr;
-        return temp;
-      }
-      else 
-      {
-        int smallest = smallestInSubTree(_ptr->right);
-        _ptr->key = smallest;
-        _ptr->right = deleteNode(_ptr->right, smallest);
+        if (_ptr->__left == NULL)
+        {
+            node * temp = _ptr->__right;
+            _alloc.deallocate(_ptr, 1);
+            return temp;
+        } 
+        else if (_ptr->__right == NULL)
+        {
+            node * temp = _ptr->__left;
+            _alloc.deallocate(_ptr, 1);
+            return temp;
+        }
+        else 
+        {
+            int smallest = smallestInSubTree(_ptr->__right);
+            _ptr->__key->second = smallest;
+            _ptr->__right = deleteNode(_ptr->__right, smallest);
         }
     }
-    // return _ptr;
     return balanceAfterDeletion(_ptr, key);
     }
     
-    node*   balanceAfterDeletion(node *_ptr, int key)
+    node*   balanceAfterDeletion(node *_ptr, value_type key)
     {
         int balance = CalculHieghtHeight(_ptr);
 
-        if (balance == 2 && CalculHieghtHeight(_ptr->left) >= 0)
+        if (balance == 2 && CalculHieghtHeight(_ptr->__left) >= 0)
             return rightRotate(_ptr);
-        else if (balance == 2 && CalculHieghtHeight(_ptr->left) == -1)
+        else if (balance == 2 && CalculHieghtHeight(_ptr->__left) == -1)
         {
-            _ptr->left = leftRotate(_ptr->left);
+            _ptr->__left = leftRotate(_ptr->__left);
             return rightRotate(_ptr);
         }   
-        else if (balance == -2 && CalculHieghtHeight(_ptr->right) <= 0)
+        else if (balance == -2 && CalculHieghtHeight(_ptr->__right) <= 0)
             return leftRotate(_ptr);
-        else if (balance == -1 && CalculHieghtHeight(_ptr->right) == 1)
+        else if (balance == -1 && CalculHieghtHeight(_ptr->__right) == 1)
         {
-            _ptr->right = rightRotate(_ptr->right);
+            _ptr->__right = rightRotate(_ptr->__right);
             return leftRotate(_ptr);
         } 
         return _ptr;
@@ -185,65 +192,67 @@ class AvlTree
             return (-1);
         else
         {
-            int _lheight = CalculHieght(_node->left);
-            int _rheight = CalculHieght(_node->right);
+            int _lheight = CalculHieght(_node->__left);
+            int _rheight = CalculHieght(_node->__right);
             return ( _lheight > _rheight ? _lheight + 1: _rheight + 1);
         }
     }   
     
     node *  rightRotate(node * y)
   {
-        node * x = y->left;
-        node * T2 = x->right;
+        node * x = y->__left;
+        node * T2 = x->__right;
     
-        x->right= y;
-        y->left = T2;
+        x->__right= y;
+        y->__left = T2;
         return x;
   }
 
     node *  leftRotate(node * x)
    {
-        node * y = x->right;
-        node * T2 = y->left;
+        node * y = x->__right;
+        node * T2 = y->__left;
     
-        y->left = x;
-        x->right = T2;
+        y->__left = x;
+        x->__right = T2;
     
         return y;
    }
     
-    node *root;
-    
+    node          *root;
+    node_pointer _alloc;
     public:
         
         AvlTree() : root(NULL)
         {
         }
         
-        node*   creatleaf(int key)
+        node*   creatleaf(value_type key)
         {
-            node* New = new node;
-            New->key = key;
-            New->left = NULL;
-            New->right = NULL;
+
+
+            node *New = _alloc.allocate(1);
+            New->__key->second = key;
+            New->__left = NULL;
+            New->__right = NULL;
             return (New);
         }
         
-        void    AddLeaf(int key)
+        void    AddLeaf(value_type key)
         {
-           root =  AddLeafPrivate(key, root);
+           root = AddLeafPrivate(key, root);
         }
         
-        node*   ReturnNode(int key, node *Ptr)
+        node*   ReturnNode(value_type key, node *Ptr)
         {
             if (Ptr)
             {
-                if (Ptr->key == key)
+                if (Ptr->__key->second == key)
                     return (Ptr);
-                if (key > Ptr->key)
-                    return ReturnNode(key, Ptr->right);
+                if (key > Ptr->__key->second)
+                    return ReturnNode(key, Ptr->__right);
                 else
-                    return (ReturnNode(key, Ptr->left));
+                    return (ReturnNode(key, Ptr->__left));
             } 
             return (NULL);  
         }
@@ -268,14 +277,14 @@ class AvlTree
             return (FindOldestPrivate(root));
         }
         
-        void    RemoveNode(int key)
+        void    RemoveNode(value_type key)
         {
             root = deleteNode(root, key);
         }
         
         int     CalculHieghtHeight(node *_ptr)
         {
-            return (root == NULL ? -1 : (CalculHieght(_ptr->left) - CalculHieght(_ptr->right))); 
+            return (root == NULL ? -1 : (CalculHieght(_ptr->__left) - CalculHieght(_ptr->__right))); 
         }    
         
         ~AvlTree()
--- Mandatory/map/map.hpp
+++ Mandatory/map/map.hpp
@@ -6,7 +6,7 @@
 /*   By: zrabhi <zrabhi@student.1337.ma >           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 23:09:03 by zrabhi            #+#    #+#             */
-/*   Updated: 2023/01/17 15:06:33 by zrabhi           ###   ########.fr       */
+/*   Updated: 2023/01/26 15:23:10 by zrabhi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,33 @@
 # include <memory>
 # include <map>
 # include <utility>
+# include "../utils/ft_utility.hpp"
+# include "../AvlTree/AvlTree.hpp"
+
 
 
 /// @todo change std::less and std::pair with my implementation
 
 namespace ft
 {
-template<class Key, class T, class Compare = std::less<Key>, class Allocator = std::allocator<std::pair<const Key, T>> >
+template<class Key, class T, class Compare = std::less<Key>, class Allocator = std::allocator<ft::pair<const Key, T>> >
  class map {
     
     public:
-            typedef std::pair<const Key, T>                     value_type;
             typedef     key                                     key_type;
             typedef     T                                       mapped_type;
-            typedef    Compare                                  key_compare;
+            typedef     ft::pair<const key_type, mapped_type>   value_type;
+            typedef     Compare                                 key_compare;
             //---- Nested fucntion to campare elements;
-            typedef     Allocator                               allocator_type;
+            typedef             Allocator                       allocator_type;
             typedef             std::size_t                     size_type; 
             typedef typename    allocator_type::reference       reference;
             typedef typename    allocator_type::const_reference const_reference;
             typedef typename    allocator_type::pointer         pointer;
             typedef typename    allocator_type::const_pointer   const_pointer;
+            typedef typename    ft::AvlTree<value_type>         ___tree;
+    private:
     };
-
-
 }
 
 
